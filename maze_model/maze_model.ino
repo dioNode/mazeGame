b7930fd1630f7d3455a358afadfa918bd40d61ca
petercondoleon <petercondoleon@gmail.com>
2018-01-14 12:07:25 +1000

    Migrated all functions into class

--- maze_model/maze_model.ino
+++ maze_model/maze_model.ino
@@ -1,86 +1,7 @@
-
-
-#define MIN_WIDTH 0
-#define MAX_WIDTH 7
-#define MIN_HEIGHT 0
-#define MAX_HEIGHT 7
-
 /*
- * Describes a point that exists with a x and y.
+ * Maze Model
  */
-struct SAPoint {
-  int x;
-  int y;
-};
 
-class SAMazeModel {
-
-  private:
-    SAPoint playerPosition = {0,0};
-
-  void setPlayerPosition(SAPoint pos) {
-    playerPosition = pos;
-  }
-
-  SAPoint getPlayerPosition() {
-    return playerPosition;
-  }
-
-
-  void movePlayerUp() {
-    playerPosition.y++;
-  }
-
-  void movePlayerDown() {
-    playerPosition.y--;
-  }
-
-  void movePlayerLeft() {
-    playerPosition.x--;
-  }
-
-  void movePlayerRight() {
-    playerPosition.x++;
-  }
-  
-};
-
-
-
-/*
- * Loads the maze from an array of bytes into an array of arrays.
- * 
- * maze - an array of bytes representing the maze for display.
- * matrix - an array of integer arrays to load maze information into.
- */
-void loadMaze(byte maze[8], int matrix[8][8]) {
-  for (int x = 0; x < 8; x++) {
-    for (int y = 0; y < 8; y++) {
-      if (maze[x] &  1<<(7-y)) {
-        matrix[x][y] = 1;
-      } else {
-        matrix[x][y] = 0;
-      }
-    }
-  }
-}
-
-/*
- * Save the maze into an array of bytes with the player.
- * 
- * matrix - an array of integer arrays to get maze information from.
- * maze - the array of bytes to save maze information to for displaying.
- */
-void saveMaze(int matrix[8][8], byte maze[8]) {
-  for (int x = 0; x < 8; x++) {
-    byte row = 0;
-    for (int y = 0; y < 8; y++) {
-      row = row << 1;
-      row |= matrix[x][y];
-    }
-    maze[x] = row;
-  }
-}
 
 // Sample maze to load
 byte sampleMaze[] {
@@ -106,15 +27,106 @@ byte outputMaze[] {
   B00000000
 };
 
+class SAMazeModel {
+  
+  /*
+   * Describes a point that exists with a x and y.
+   */
+  struct SAPoint {
+    int x;
+    int y;
+  };
+
+  private:
+  
+  SAPoint playerPosition = {0,0};
+
+  public:
+  
+  void setPlayerPosition(SAPoint pos) {
+    playerPosition = pos;
+  }
+
+  SAPoint getPlayerPosition() {
+    return playerPosition;
+  }
+
+  void movePlayerUp() {
+    playerPosition.y++;
+  }
+
+  void movePlayerDown() {
+    playerPosition.y--;
+  }
+
+  void movePlayerLeft() {
+    playerPosition.x--;
+  }
+
+  void movePlayerRight() {
+    playerPosition.x++;
+  }
+
+  /*
+   * Loads the maze from an array of bytes into an array of arrays.
+   * 
+   * maze - an array of bytes representing the maze for display.
+   * matrix - an array of integer arrays to load maze information into.
+   */
+  void loadMaze(byte maze[8], int matrix[8][8]) {
+    for (int x = 0; x < 8; x++) {
+      for (int y = 0; y < 8; y++) {
+        if (maze[x] &  1<<(7-y)) {
+          matrix[x][y] = 1;
+        } else {
+          matrix[x][y] = 0;
+        }
+      }
+    }
+  }
+
+  /*
+   * Save the maze into an array of bytes with the player.
+   * 
+   * matrix - an array of integer arrays to get maze information from.
+   * maze - the array of bytes to save maze information to for displaying.
+   */
+  void saveMaze(int matrix[8][8], byte maze[8]) {
+    for (int x = 0; x < 8; x++) {
+      byte row = 0;
+      for (int y = 0; y < 8; y++) {
+        row = row << 1;
+        row |= matrix[x][y];
+      }
+      maze[x] = row;
+    }
+  }
+
+  /*
+   * Prints a maze to the console
+   */
+  void printMaze(byte maze[8]) {
+    Serial.println();
+    for (int row = 0; row < 8; row++) {
+      Serial.println(maze[row], BIN);
+    }
+    Serial.println();
+  }
+
+};
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Maze Model");
-  
+
+  SAMazeModel mazeModel = SAMazeModel();
   int arrayMaze[8][8];              // Stores the maze representation
-  loadMaze(sampleMaze, arrayMaze);  // Load the sample maze into the arrayMaze
+  mazeModel.loadMaze(sampleMaze, arrayMaze);  // Load the sample maze into the arrayMaze
   arrayMaze[4][4] = 1;              // Make a change to the maze
-  saveMaze(arrayMaze, outputMaze);  // Save the arrayMaze to the output maze
+  mazeModel.saveMaze(arrayMaze, outputMaze);  // Save the arrayMaze to the output maze
   
     
 }
@@ -124,16 +136,6 @@ void loop() {
 }
 
 
-/*
- * Prints a maze to the console
- */
-void printMaze(byte maze[8]) {
-  Serial.println();
-  for (int row = 0; row < 8; row++) {
-    Serial.println(maze[row], BIN);
-  }
-  Serial.println();
-}
 
 
 
