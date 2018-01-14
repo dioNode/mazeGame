cbc2db31e454b601c8d87b2ce0de382414f76949
petercondoleon <petercondoleon@gmail.com>
2018-01-14 13:43:59 +1000

    Bruce Made Error

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,130 @@
 
 LedControl lc=LedControl(12,11,10,1);  // Pins: DIN,CLK,CS, # of Display connected
 SSDTimer timer = SSDTimer();
-SAMazeModel player;
 
 unsigned long delayTime=300;  // Delay between Frames
 unsigned long lastTick;
+
+
+class SAMazeModel {
+
+  public:
+  
+  /*
+   * Describes a point that exists with a x and y.
+   */
+  struct SAPoint {
+    int x;
+    int y;
+  };
+  
+  SAPoint playerPosition = {0,0};
+
+  int mazeMatrix[8][8];  // Stores the maze representation
+
+  void movePlayerUp() {
+    if (wallAt(playerPosition.x, playerPosition.y-1)) {
+      // Wall at this location
+    } else {
+      // No wall at this location
+      playerPosition.y--;
+    }
+  }
+
+  void movePlayerDown() {
+    if (wallAt(playerPosition.x, playerPosition.y+1)) {
+      // Wall at this location
+    } else {
+      // No wall at this location
+      playerPosition.y++;
+    }
+  }
+
+  void movePlayerLeft() {
+    if (wallAt(playerPosition.x-1, playerPosition.y)) {
+      // Wall at this location
+    } else {
+      // No wall at this location
+      playerPosition.x--;
+    }
+  }
+
+  void movePlayerRight() {
+    if (wallAt(playerPosition.x+1, playerPosition.y)) {
+      // Wall at this location
+    } else {
+      // No wall at this location
+      playerPosition.x++;
+    }
+  }
+
+  /*
+   * Checks the matrix for a wall at the specified position
+   */
+  bool wallAt(int x, int y) {
+    return mazeMatrix[x][y] == 1;
+  }
+
+  /*
+   * Moves the player to an empty spot.
+   */
+  void spawnPlayer() {
+    if (mazeMatrix[0][0] == 0) {
+      playerPosition = {0, 0};
+    } else if (mazeMatrix[7][0] == 0) {
+      playerPosition = {7, 0};
+    } else if (mazeMatrix[0][7] == 0) {
+      playerPosition = {0, 7};
+    } else if (mazeMatrix[7][7] == 0) {
+      playerPosition = {7, 7};
+    }
+  }
+
+  /*
+   * Loads the maze from an array of bytes into an array of arrays.
+   * 
+   * maze - an array of bytes representing the maze for loading into the matix.
+   */
+  void loadMaze(byte maze[8]) {
+    for (int x = 0; x < 8; x++) {
+      for (int y = 0; y < 8; y++) {
+        if (maze[x] &  1<<(7-y)) {
+          mazeMatrix[x][y] = 1;
+        } else {
+          mazeMatrix[x][y] = 0;
+        }
+      }
+    }
+  }
+
+  /*
+   * Save the maze into an array of bytes with the player.
+   * 
+   * maze - the array of bytes to save maze information to for displaying.
+   */
+  void saveMaze(byte maze[8]) {
+    for (int x = 0; x < 8; x++) {
+      byte row = 0;
+      for (int y = 0; y < 8; y++) {
+        row = row << 1;
+        row |= mazeMatrix[x][y];
+      }
+      maze[x] = row;
+    }
+  }
+
+  /*
+   * Prints a maze to the console
+   */
+  void printMaze(byte maze[8]) {
+    Serial.println();
+    for (int row = 0; row < 8; row++) {
+      Serial.println(maze[row], BIN);
+    }
+    Serial.println();
+  }
+
+};
 
 void setup() {
   lc.shutdown(0,false);  // Wake up displays
@@ -16,9 +136,7 @@ void setup() {
   lc.clearDisplay(0);  // Clear Displays
   lc.clearDisplay(1);
   lastTick = millis();
-  player = SAMAzeModel();
   playFrame();
-  
 }
 
 byte mazeWalls[] {
@@ -113,7 +231,8 @@ void loop() {
   unsigned long endTime;
   unsigned long startTime = millis();
   playFrame();
-  while (true) {
+  SAMazeModel player = SAMazeModel();
+  while (true) { // while the game is still going
     if (millis()-lastTick > 500) {
       togglePlayerPosition(player);
       lastTick = millis();
